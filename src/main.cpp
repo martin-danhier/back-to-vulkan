d2882a7870440996be637ad30759c4cf1b1d2e55
Martin Danhier <martin.danhier@student.unamur.be>
2021-03-15 01:18:26 +0100

    added dependencies

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <glm/glm.hpp>
+#include <tiny_obj_loader.h>
+#include <stb_image.h>
+#include <vulkan/vulkan_core.h>
+#include <VkBootstrap.h>
+#include <vk_mem_alloc.h>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
